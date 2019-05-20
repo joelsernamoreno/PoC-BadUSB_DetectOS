a9f390de0ab61f6f1326ac22d4ce84758cdf510e
joelsernamoreno <joelsernamoreno@gmail.com>
2019-05-20 20:55:03 +0200

    PoC

---
+++ PoC-BadUSB_DetectOS.ino
@@ -0,0 +1,86 @@
+/*
+ *  Basic hardware keylogger
+ *
+ *  Explanation: Simple code to execute payloads depending on the detected OS.
+ * 
+ *
+ *  Copyright (C) 2019 Joel Serna & Ernesto Sanchez
+ *
+ *  Version:  1.0
+ *  Design:   Joel Serna & Ernesto Sanchez
+ *  Implementation: Joel Serna & Ernesto Sanchez
+ *  Library:  @keyboardio
+ *
+ */
+
+#include "FingerprintUSBHost.h"
+#include "Keyboard.h"
+
+void setup() {
+
+  Keyboard.begin();
+  String os;
+  delay(3000);
+
+  FingerprintUSBHost.guessHostOS(os);
+	
+	if(os == "Windows") {
+		Serial.print("The Host OS is: ");
+		Serial.println(os);
+
+    // Hello World in Windows.
+		Keyboard.press(KEY_LEFT_GUI);
+		Keyboard.press('r');
+		delay(100);
+		Keyboard.releaseAll();
+		delay(2000);
+		Keyboard.print("notepad");
+    delay(1000);
+    Keyboard.press(KEY_RETURN);
+    delay(100);
+    Keyboard.releaseAll();
+		delay(3000);
+		Keyboard.print("Hello World");		
+	}
+	
+	else if(os == "Linux") {
+		Serial.print("The Host OS is: ");
+		Serial.println(os);
+
+    // Hello World in Linux.
+		Keyboard.press(KEY_LEFT_ALT);
+		Keyboard.press(KEY_F2);
+		delay(100);
+		Keyboard.releaseAll();
+		delay(2000);
+		Keyboard.print("gedit");
+		delay(1000);
+    Keyboard.press(KEY_RETURN);
+    delay(100);
+    Keyboard.releaseAll();
+    delay(3000);
+		Keyboard.print("Hello World");
+	}
+	
+	else if(os == "MacOS") {
+		Serial.print("The Host OS is: ");
+		Serial.println(os);
+
+    // Hello World in MacOS.
+		Keyboard.press(KEY_LEFT_GUI);
+		Keyboard.press(' ');
+		delay(100);
+		Keyboard.releaseAll();
+		delay(2000);
+		Keyboard.print("Terminal");
+    delay(1000);
+    Keyboard.press(KEY_RETURN);
+    delay(100);
+    Keyboard.releaseAll();
+		delay(3000);
+		Keyboard.print("Hello World");
+	}
+}
+void loop() {
+    // put your main code here, to run repeatedly:
+}
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # PoC-BadUSB_DetectOS
-Simple code to execute payloads depending on the detected operating system.
+Simple code to execute payloads depending on the detected OS.
+
+Thanks to @keyboardio for the development and publication of the FingerprintUSBHost library.
+
+## Hardware requirements:
+
+Arduino Leonardo, ProMicro, Lilypad USB, etc.
+
+## Software requirements:
+
+1. IDE arduino: http://arduino.cc
+2. Keyboard library
+3. FingerprintUSBHost library
+4. Basic example code: https://github.com/joelsernamoreno/PoC-BadUSB_DetectOS
+
+## Instalation:
+
+1. Download the arduino IDE and this repository.
+2. Copy the Keyboard library to the arduino library directory.
+3. Copy the FingerprintUSBHost library to the arduino libary directory.
+4. Open the arduino IDE
+5. Select the board (Tools -> Board) example: Tools -> Board -> LilyPad Arduino USB
+6. Open sketch PoC-BadUSB_DetectOS.ino
+7. Compile and upload.
+
+More payloads: https://github.com/joelsernamoreno/badusb_examples/tree/master/atmega32u4_arduino_leonardo
+
+If you have any questions, you can contact us by twitter:
+@ernesto_xload @JoelSernaMoreno
